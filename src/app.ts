d8d445c06bac5768b5aa0ebf2ee1ec0c25afedcb
odan <odan3240@gmail.com>
2020-08-13 00:51:12 +0900

    add typing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,77 @@
-async function main(): Promise<void> {
-  console.log("poyo");
+import Vue, { ComponentOptions } from "vue";
+import { CombinedVueInstance } from "../node_modules/vue/types/vue";
+
+type DataDef<Data, Props, V> = Data | ((this: Readonly<Props> & V) => Data);
+
+type DefaultAsyncData<V> =
+  | object
+  | ((this: V, context: any) => Promise<object | void> | object | void);
+
+type ThisTypedComponentOptionsWithArrayPropsAndAsyncData<
+  V extends Vue,
+  Data,
+  Methods,
+  Computed,
+  PropNames extends string,
+  AsyncData
+> = object &
+  ComponentOptions<
+    V,
+    DataDef<Data, Record<PropNames, any>, V>,
+    Methods,
+    Computed,
+    PropNames[],
+    Record<PropNames, any>,
+    DataDef<AsyncData, Record<PropNames, any>, V>
+  > &
+  ThisType<
+    CombinedVueInstance<
+      V,
+      Data,
+      Methods,
+      Computed,
+      Readonly<Record<PropNames, any>>
+    > &
+      AsyncData
+  >;
+
+declare module "vue/types/options" {
+  interface ComponentOptions<
+    V extends Vue,
+    Data = DefaultData<V>,
+    Methods = DefaultMethods<V>,
+    Computed = DefaultComputed,
+    PropsDef = PropsDefinition<DefaultProps>,
+    Props = DefaultProps,
+    AsyncData = DefaultAsyncData<V>
+  > {
+    asyncData?: AsyncData;
+  }
 }
 
-main();
+declare module "vue/types/vue" {
+  interface VueConstructor<V extends Vue> {
+    extend<Data, Methods, Computed, PropNames extends string, AsyncData>(
+      options?: ThisTypedComponentOptionsWithArrayPropsAndAsyncData<
+        V,
+        Data,
+        Methods,
+        Computed,
+        PropNames,
+        AsyncData
+      >
+    ): ExtendedVue<V, Data, Methods, Computed, Record<PropNames, any>>;
+  }
+}
 
-process.on("unhandledRejection", (reason) => {
-  console.error(reason);
-  process.exit(1);
+export default Vue.extend({
+  asyncData() {
+    return { a: 10 };
+  },
+  data: () => ({ b: 10 }),
+  methods: {
+    click(): void {
+      console.log(this.a);
+    },
+  },
 });
